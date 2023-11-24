28a096d2d05ec943c4f8140e312f4cfe6798752f
Juan Carlos <al2192000778@azc.uam.mx>
2023-11-24 12:11:15 -0600

    cambios main

--- main.js
+++ main.js
@@ -9,4 +9,12 @@ app.get('/alumnos', (req,resp)=> {
     resp.json({nombre:"Juan",edad:26})
 })
 
+app.get('/grupo', (req,resp)=> {
+    resp.json({nombre:"E306",capacidad:26})
+})
+
+app.get('/profesores', (req,resp)=> {
+    resp.json({nombre:"Maricela CLaudia Bravo"})
+})
+
 app.listen(3000, ()=> console.log("Servidor corriendo en el puerto 3000"));
